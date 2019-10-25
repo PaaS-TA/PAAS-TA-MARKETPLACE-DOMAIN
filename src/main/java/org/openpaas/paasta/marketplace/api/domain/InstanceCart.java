aa519bb6fb60496eb1d587719f10b92fb1c4d32e
dayworker <youngjeachoi@gmail.com>
2019-10-25 17:22:23 +0900

    사용자포탈 상품목록상세 작업

---
+++ src/main/java/org/openpaas/paasta/marketplace/api/domain/InstanceCart.java
@@ -0,0 +1,82 @@
+package org.openpaas.paasta.marketplace.api.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class InstanceCart extends AbstractEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @NotNull
+    private Software software;
+    
+    private String softwarePlanId;
+
+    private String appName;
+
+    private String appGuid;
+
+    private String appUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private ProvisionStatus provisionStatus;
+
+    private Integer provisionTryCount = 0;
+
+    private LocalDateTime provisionStartDate;
+
+    private LocalDateTime provisionEndDate;
+
+    @Enumerated(EnumType.STRING)
+    private ProvisionStatus deprovisionStatus;
+
+    private Integer deprovisionTryCount = 0;
+
+    private LocalDateTime deprovisionStartDate;
+
+    private LocalDateTime deprovisionEndDate;
+
+    private LocalDateTime lastStatusCheckDate;
+
+    private LocalDateTime usageStartDate;
+
+    private LocalDateTime usageEndDate;
+
+    private String host;
+    
+    public enum Status {
+        // provision
+        Approval,
+        // deprovision
+        Deleted
+    }
+
+    public enum ProvisionStatus {
+        Pending, Ready, InProgress, Successful, Failed
+    }
+    
+    @PrePersist
+    @Override
+    public void prePersist() {}
+}
---
+++ src/main/java/org/openpaas/paasta/marketplace/api/domain/InstanceCartSpecification.java
@@ -0,0 +1,151 @@
+package org.openpaas.paasta.marketplace.api.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.openpaas.paasta.marketplace.api.domain.Instance.Status;
+import org.openpaas.paasta.marketplace.api.domain.Software.Type;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import lombok.Data;
+
+@Data
+public class InstanceCartSpecification implements Specification<InstanceCart> {
+
+    private static final long serialVersionUID = 1L;
+
+    private static String systemHost;
+
+    private String host;
+
+    private Yn inUse;
+
+    private Status status;
+
+    private String createdBy;
+
+    private Long categoryId;
+
+    private Long softwareId;
+
+    private String softwareNameLike;
+
+    private Type softwareType;
+
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDateTime usageStartDate;
+
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDateTime usageEndDate;
+
+    private Instance.ProvisionStatus provisionStatus;
+
+    private List<Instance.ProvisionStatus> provisionStatusIn = new ArrayList<>();
+
+    private Integer provisionTryCountMax;
+
+    private LocalDateTime provisionStartDateBefore;
+
+    private Instance.ProvisionStatus deprovisionStatus;
+
+    private List<Instance.ProvisionStatus> deprovisionStatusIn = new ArrayList<>();
+
+    private Integer deprovisionTryCountMax;
+
+    private LocalDateTime deprovisionStartDateBefore;
+ 
+    public static void setSystemHost(String systemHost) {
+        InstanceCartSpecification.systemHost = systemHost;
+    }
+
+    public static InstanceCartSpecification ofSystemHost() {
+        InstanceCartSpecification spec = new InstanceCartSpecification();
+        spec.setHost(systemHost);
+        return spec;
+    }
+    
+    @Override
+    public Predicate toPredicate(Root<InstanceCart> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        List<Predicate> restrictions = new ArrayList<>();
+        if (inUse != null) {
+            restrictions.add(builder.equal(root.get("inUse"), inUse));
+        }
+        if (status != null) {
+            restrictions.add(builder.equal(root.get("status"), status));
+        }
+        if (createdBy != null) {
+            restrictions.add(builder.equal(root.get("createdBy"), createdBy));
+        }
+        if (categoryId != null) {
+            restrictions.add(builder.equal(root.get("software").get("category").get("id"), categoryId));
+        }
+        if (softwareId != null) {
+            restrictions.add(builder.equal(root.get("software").get("id"), softwareId));
+        }
+        if (softwareNameLike != null) {
+            restrictions.add(builder.like(root.get("software").get("name"), "%" + softwareNameLike + "%"));
+        }
+        if (softwareType != null) {
+            restrictions.add(builder.equal(root.get("software").get("type"), softwareType));
+        }
+
+        if (usageStartDate != null && usageEndDate != null) {
+            Predicate predicate =
+                    builder.or(
+                            builder.and(
+                                builder.lessThanOrEqualTo(root.get("usageStartDate"),
+                                        usageStartDate),
+                                builder.or(
+                                        builder.isNull(root.get("usageEndDate")),
+                                        builder.greaterThanOrEqualTo(root.get("usageEndDate"),
+                                                usageStartDate)
+                                )
+                            ),
+                            builder.and(
+                                builder.greaterThanOrEqualTo(root.get("usageStartDate"),
+                                        usageStartDate),
+                                builder.lessThan(root.get("usageStartDate"), usageEndDate)
+                            )
+                        );
+            restrictions.add(predicate);
+        }
+
+        if (provisionStatus != null) {
+            restrictions.add(builder.equal(root.get("provisionStatus"), provisionStatus));
+        }
+        if (provisionStatusIn != null && !provisionStatusIn.isEmpty()) {
+            restrictions.add(root.get("provisionStatus").in(provisionStatusIn));
+        }
+        if (provisionTryCountMax != null) {
+            restrictions.add(builder.lessThanOrEqualTo(root.get("provisionTryCount"), provisionTryCountMax));
+        }
+        if (provisionStartDateBefore != null) {
+            restrictions.add(builder.lessThan(root.get("provisionStartDate"), provisionStartDateBefore));
+        }
+        if (deprovisionStatus != null) {
+            restrictions.add(builder.equal(root.get("deprovisionStatus"), deprovisionStatus));
+        }
+        if (deprovisionStatusIn != null && !deprovisionStatusIn.isEmpty()) {
+            restrictions.add(root.get("deprovisionStatus").in(deprovisionStatusIn));
+        }
+        if (deprovisionTryCountMax != null) {
+            restrictions.add(builder.lessThanOrEqualTo(root.get("deprovisionTryCount"), deprovisionTryCountMax));
+        }
+        if (deprovisionStartDateBefore != null) {
+            restrictions.add(builder.lessThan(root.get("deprovisionStartDate"), deprovisionStartDateBefore));
+        }
+        if (host != null) {
+            restrictions.add(builder.equal(root.get("host"), host));
+        }
+
+        return builder.and(restrictions.toArray(new Predicate[]{}));
+    }
+}
